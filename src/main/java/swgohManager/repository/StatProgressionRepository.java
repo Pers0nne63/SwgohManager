8d77c1c76b766893661412e9577506387878b028
BenjaminMOREL <BenjaminMOREL@DC_SUPPORT>
2026-08-28 15:44:10 +0200

    Correction bug

--- src/main/java/swgohManager/repository/StatProgressionRepository.java
+++ src/main/java/swgohManager/repository/StatProgressionRepository.java
@@ -8,5 +8,4 @@ import swgohManager.model.StatProgression;
 
 public interface StatProgressionRepository extends JpaRepository<StatProgression, Long> {
 	List<StatProgression> findByStatProgressionIdIn(List<String> ids);
-	void deleteByPlayerIdNotIn(List<String> activePlayerIds);
 }
